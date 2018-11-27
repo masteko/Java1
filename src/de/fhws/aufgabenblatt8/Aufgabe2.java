38864cdb405d26988da7640af5a3449e14251cfb
Manuel <njester5030@gmail.com>
2018-11-27 14:29:33 +0100

    handling of incorrect inputs added

--- src/de/fhws/aufgabenblatt8/Aufgabe2.java
+++ src/de/fhws/aufgabenblatt8/Aufgabe2.java
@@ -10,16 +10,26 @@ public class Aufgabe2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Eingabe der Zahl:");
-        char[] numberArray = scanner.nextLine().toCharArray();
-        System.out.println("Eingabe der Sterne:");
-        int stars = scanner.nextInt();
+        int number = scanner.nextInt();
 
-        for (int i = numberArray.length - 1; i >= 0 ; i--) {
-            System.out.print(numberArray[i]);
-
-            if (i == 0) break;
-            for (int j = 0; j < stars; j++) {
-                System.out.print("*");
+        if (number <= 0) {
+            System.out.println("Zahl muss groesser null sein");
+        } else {
+            char[] numberArray = String.valueOf(number).toCharArray();
+            System.out.println("Eingabe der Sterne:");
+            int stars = scanner.nextInt();
+    
+            if (stars < 0) {
+                System.out.println("Zahl muss groesser gleich null sein");
+            } else {
+                for (int i = numberArray.length - 1; i >= 0 ; i--) {
+                    System.out.print(numberArray[i]);
+        
+                    if (i == 0) break;
+                    for (int j = 0; j < stars; j++) {
+                        System.out.print("*");
+                    }
+                }
             }
         }
 
