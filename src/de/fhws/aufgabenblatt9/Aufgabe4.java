285336ca779b4f381c5c7222eec9e8ee3c583881
Manuel <njester5030@gmail.com>
2018-12-04 21:18:16 +0100

    task 4 added

---
+++ src/de/fhws/aufgabenblatt9/Aufgabe4.java
@@ -0,0 +1,31 @@
+package de.fhws.aufgabenblatt9;
+
+/**
+ * Aufgabe4
+ */
+public class Aufgabe4 {
+
+    public static void main(String[] args) {
+        char[] a = {'c', 'd', 'e'};
+        char[] b = {'1', 'u'};
+        char[] aundb = konkatenieren(a, b);
+
+        for (int i = 0; i < aundb.length; i++) {
+            System.out.println(aundb[i]);
+        }
+    }
+
+    public static char[] konkatenieren(char[] a, char[] b) {
+        char[] result = new char[a.length + b.length];
+
+        for (int i = 0; i < a.length; i++) {
+            result[i] = a[i];
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            result[a.length - 1 + i] = b[i];
+        }
+
+        return result;
+    }
+}
