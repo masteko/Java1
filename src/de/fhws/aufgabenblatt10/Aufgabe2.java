8b9c6dd02f4d1baacaeed150630258a4b6269d92
Manuel <njester5030@gmail.com>
2018-12-10 21:40:31 +0100

    task 2 added

---
+++ src/de/fhws/aufgabenblatt10/Aufgabe2.java
@@ -0,0 +1,15 @@
+package de.fhws.aufgabenblatt10;
+
+/**
+ * Aufgabe2
+ */
+public class Aufgabe2 {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Bitte uebergebe genau 2 Zahlen");
+        } else {
+            System.out.println(Integer.valueOf([0]) * Integer.valueOf(args[1]));
+        }
+    }
+}
