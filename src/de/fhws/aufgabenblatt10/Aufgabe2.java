93b1fa9e8ebde70ad581d94d3dbf5a51f473dedb
Manuel <njester5030@gmail.com>
2018-12-10 21:41:23 +0100

    error fixed

--- src/de/fhws/aufgabenblatt10/Aufgabe2.java
+++ src/de/fhws/aufgabenblatt10/Aufgabe2.java
@@ -9,7 +9,7 @@ public class Aufgabe2 {
         if (args.length != 2) {
             System.out.println("Bitte uebergebe genau 2 Zahlen");
         } else {
-            System.out.println(Integer.valueOf([0]) * Integer.valueOf(args[1]));
+            System.out.println(Integer.valueOf(args[0]) * Integer.valueOf(args[1]));
         }
     }
 }
