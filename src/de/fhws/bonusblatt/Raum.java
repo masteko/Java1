b7abc896e1ca8a9e3d5d9b5607eb924fdbf8492c
Manuel <njester5030@gmail.com>
2019-01-14 11:50:59 +0100

    raum returned

---
+++ src/de/fhws/bonusblatt/Raum.java
@@ -0,0 +1,35 @@
+package de.fhws.bonusblatt;
+
+/**
+ * Raum
+ */
+public class Raum {
+
+    private String kennung;
+    private int maxSitzplaetze;
+    private boolean belegt; //wird durch Aufgabe 4 überflüssig
+    private Vorlesung vorlesung;
+
+    public Raum(String kennung, int maxSitzplaetze) {
+        this.kennung = kennung;
+        this.maxSitzplaetze = maxSitzplaetze;
+    }
+
+    public String getKennung() {
+        return this.kennung;
+    }
+
+    public void belegeRaum(Vorlesung v) {
+        this.belegt = true; //wird durch Aufgabe 4 überflüssig
+        this.vorlesung = v;
+    }
+
+    public void gebeRaumFrei() {
+        this.belegt = false; //wird durch Aufgabe 4 überflüssig
+        this.vorlesung = null;
+    }
+
+    public Vorlesung istRaumBelegt() {
+        return this.vorlesung;
+    }
+}
