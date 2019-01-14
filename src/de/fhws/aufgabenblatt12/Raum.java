4c6a06af741968af9bb11af4eac14ef4f3f25823
Manuel <njester5030@gmail.com>
2019-01-14 12:02:22 +0100

    raum fixed

--- src/de/fhws/aufgabenblatt12/Raum.java
+++ src/de/fhws/aufgabenblatt12/Raum.java
@@ -9,10 +9,35 @@ public class Raum {
     private int maxSitzplaetze;
     private boolean belegt; //wird durch Aufgabe 4 überflüssig
     private Vorlesung vorlesung;
+    private Student[] studenten;
+    private int belegteSitzplaetze;
 
     public Raum(String kennung, int maxSitzplaetze) {
         this.kennung = kennung;
         this.maxSitzplaetze = maxSitzplaetze;
+        this.studenten = new Student[maxSitzplaetze];
+    }
+
+    public void betreteRaum(Student s) {
+        if (belegteSitzplaetze < maxSitzplaetze) {
+            for (int i = 0; i < studenten.length; i++) {
+                if (studenten[i] != null) {
+                    studenten[i] = s;
+                    belegteSitzplaetze++;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void verlasseRaum(Student s) {
+        for (int i = 0; i < studenten.length; i++) {
+            if (studenten[i] == s) {
+                studenten[i] = null;
+                belegteSitzplaetze--;
+                break;
+            }
+        }
     }
 
     public String getKennung() {
@@ -31,5 +56,19 @@ public class Raum {
 
     public Vorlesung istRaumBelegt() {
         return this.vorlesung;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+
+        for (int i = 0; i < studenten.length; i++) {
+            if (studenten[i] != null) {                
+                result += (studenten[i].getName() + " ");
+                System.out.println("hallo");
+            }
+        }
+
+        return result;
     }
 }
