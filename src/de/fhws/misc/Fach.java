035d7ac52d40bc1fce199c799924ed3defe717ed
Manuel <njester5030@gmail.com>
2019-01-23 16:33:00 +0100

    fach added

---
+++ src/de/fhws/misc/Fach.java
@@ -0,0 +1,41 @@
+package de.fhws.misc;
+
+/**
+ * Fach
+ */
+public class Fach {
+    private int number;
+    private boolean open;
+
+    public Fach(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    @Override
+    public String toString() {
+        return open ? "o" : "z";
+
+        //alternative:
+        // if (open) {
+        //     return "o";
+        // } else {
+        //     return "z";
+        // }
+    }
+}
